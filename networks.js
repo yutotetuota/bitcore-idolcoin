6bc3180a0cb1aef05c6b68ef0c6e4a0c104a2fbd
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-10-11 21:48:16 +0900

    Fix NEETCOIN to IDOLCOIN for Mainnet(Livenet)

--- networks.js
+++ networks.js
@@ -7,25 +7,25 @@ var hex = function(hex) {
 exports.livenet = {
   name: 'livenet',
   magic: hex('FAF2EFB4'),
-  addressVersion: 53,
-  privKeyVersion: 181,
-  P2SHVersion: 112,
+  addressVersion: 102,
+  privKeyVersion: 103,
+  P2SHVersion: 103,
   hkeyPublicVersion: 0x0,
   hkeyPrivateVersion: 0x0,
   genesisBlock: {
-    hash: hex('00000da3701e9b6eb1afa31d97ce6fc2c09cb7e165271b2e900a4275df8dd5bb'),
-    merkle_root: hex('0a8875e41a01a01dd0f1c759d5da353ee92561c089d19ef939510d6eda44c096'),
+    hash: hex('000000429869eb624d77c9ea5ecc9abbc3f40fd2d7760ed93e1f4070fd36681d'),
+    merkle_root: hex('2e953947cd107664f5a0bdc5ea99c3ce4537ed66cf04eb46eac7284e3b790a2e'),
     height: 0,
-    nonce: 0,
+    nonce: 3677645,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1511862256,
+    timestamp: 1538040072,
     bits: 0x1e0fffff,
   },
   dnsSeeds: [
-    'dnsseed.neetcoin.jp'
+    '13.115.89.54'
   ],
-  defaultClientPort: 21011
+  defaultClientPort: 32338
 };
 
 exports.mainnet = exports.livenet;
